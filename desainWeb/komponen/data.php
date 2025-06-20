9d4ab365cddd8a854d05c597fd6a5574d7d9690b
muhRidhoni <mridhoni2233@gmail.com>
2025-06-20 08:58:42 +0700

    Add files via upload

---
+++ desainWeb/komponen/data.php
@@ -0,0 +1,18 @@
+<?php
+    $prodi = [
+        'Teknologi Informasi' => 31,
+        'Akuntansi'           => 26,
+        'Budidaya Ikan'       => 22
+    ];
+
+    $gender = [
+        'Laki-Laki' => 50,
+        'Perempuan' => 29
+    ];
+
+    $tahun = [
+        '2023' => 42,
+        '2024' => 34,
+        '2025' => 70
+    ];
+?>
